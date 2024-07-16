3c71cb49fe4820d183b9d5cf5c411693acba4349
Emma Forner <emma@emmafm.net>
2024-07-16 18:08:29 +0200

    Exercise 4.4

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -8,3 +8,37 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('totalLikes', () => {
+    test('of an empty list', () => {
+        assert.strictEqual(listHelper.totalLikes([]), 0)
+    })
+
+    test('of a one element list', () => {
+        assert.strictEqual(listHelper.totalLikes([
+            {
+                'title': 'Blog1',
+                'author': 'Author1',
+                'url': 'url1',
+                'likes': 10
+            }
+        ]), 10)
+    })
+
+    test('of a two elements list', () => {
+        assert.strictEqual(listHelper.totalLikes([
+            {
+                'title': 'Blog1',
+                'author': 'Author1',
+                'url': 'url1',
+                'likes': 10
+            },
+            {
+                'title': 'Blog2',
+                'author': 'Author2',
+                'url': 'url2',
+                'likes': 20
+            }
+        ]), 30)
+    })
+})
