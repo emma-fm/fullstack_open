cdfe24d4405170dededab2cc8fc36f8ec692c6c8
Emma Forner <emma@emmafm.net>
2024-07-16 18:15:25 +0200

    Exercise 4.5

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -2,6 +2,20 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const testBlog1 = {
+    'title': 'Blog1',
+    'author': 'Author1',
+    'url': 'url1',
+    'likes': 10
+}
+
+const testBlog2 = {
+    'title': 'Blog2',
+    'author': 'Author2',
+    'url': 'url2',
+    'likes': 20
+}
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -15,30 +29,24 @@ describe('totalLikes', () => {
     })
 
     test('of a one element list', () => {
-        assert.strictEqual(listHelper.totalLikes([
-            {
-                'title': 'Blog1',
-                'author': 'Author1',
-                'url': 'url1',
-                'likes': 10
-            }
-        ]), 10)
+        assert.strictEqual(listHelper.totalLikes([testBlog1]), 10)
     })
 
     test('of a two elements list', () => {
-        assert.strictEqual(listHelper.totalLikes([
-            {
-                'title': 'Blog1',
-                'author': 'Author1',
-                'url': 'url1',
-                'likes': 10
-            },
-            {
-                'title': 'Blog2',
-                'author': 'Author2',
-                'url': 'url2',
-                'likes': 20
-            }
-        ]), 30)
+        assert.strictEqual(listHelper.totalLikes([testBlog1, testBlog2]), 30)
+    })
+})
+
+describe('favoriteBlog', () => {
+    test('of an empty list', () => {
+        assert.deepStrictEqual(listHelper.favoriteBlog([]), 0)
+    })
+
+    test('of a one element list', () => {
+        assert.deepStrictEqual(listHelper.favoriteBlog([testBlog1]), testBlog1)
+    })
+
+    test('of a two elements list', () => {
+        assert.deepStrictEqual(listHelper.favoriteBlog([testBlog1, testBlog2]), testBlog2)
     })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,7 +8,14 @@ const totalLikes = (blogs) => {
         blogs.reduce((sum, item) => sum +  item.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.length === 0 ?
+        0 :
+        blogs.reduce((curr, item) => item.likes > curr.likes ? item : curr, blogs[0])
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
