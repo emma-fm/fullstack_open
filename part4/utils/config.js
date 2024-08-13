eaf762420333259eefa11a0dcee4ee21ed435e2f
Emma Forner <emma@emmafm.net>
2024-08-13 18:03:46 +0200

    Exercise 4.18

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.MONGODB_URI_TEST : process.env.MONGODB_URI_PROD
 const PORT = 3003
+const SECRET = process.env.SECRET
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  SECRET
 }
